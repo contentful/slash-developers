89a612968c6b8ed662524fb2bbbbe308c3dbc7f1
Chris Ward <chriswhward@gmail.com>
2017-02-16 13:32:52 +0100

    Check spelling on only content that has changed

--- spelling.sh
+++ spelling.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-wc -l Spelling-and-style-sheet/.hunspell_en_US_contentful > Spelling-and-style-sheet/contentful.dic
-sort Spelling-and-style-sheet/.hunspell_en_US_contentful | uniq >> Spelling-and-style-sheet/contentful.dic
-touch Spelling-and-style-sheet/contentful.aff
-sudo cp contentful.* /usr/share/hunspell/
+mdspell -r -n -a --en-us `git diff --name-only HEAD HEAD~5`
 
-find . -iname '*.md' -type f -exec hunspell -d en_US,contentful -l {} \;
+# wc -l Spelling-and-style-sheet/.spelling > Spelling-and-style-sheet/contentful.dic
+# sort Spelling-and-style-sheet/.spelling | uniq >> Spelling-and-style-sheet/contentful.dic
+# touch Spelling-and-style-sheet/contentful.aff
+# sudo cp contentful.* /usr/share/hunspell/
+
+# find . -iname '*.md' -type f -exec hunspell -d en_US,contentful -l {} \;
